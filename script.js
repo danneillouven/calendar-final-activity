8ffe98ec0758dbba74608e2e4afad0f9bf1620ef
psyxph <danneillouven@gmail.com>
2023-11-30 15:13:37 +0800

    modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay
 const monthDays = document.querySelector(".date");
 const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
-const nextDays = 7 + lastDayIndex - 1;
+const nextDays = 7 - lastDayIndex - 1;
+console.log(lastDayIndex);
 const months = [
     "January",
     "February",
@@ -26,11 +27,17 @@ document.querySelector(".current-year").innerHTML = date.getFullYear();
 
 let days = "";
 
-for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div>${prevLastDay - x + 1}</div>`;
+for (let i = firstDayIndex; i > 0; i--) {
+    days += `<div>${prevLastDay - i + 1}</div>`;
 }
 
 for (let i = 1; i <= lastDay; i++) {
+    days += `<div>${i}</div>`;
+}
+
+
+
+for (let i = 1; i <= nextDays; i++) {
     days += `<div>${i}</div>`;
 }
 
